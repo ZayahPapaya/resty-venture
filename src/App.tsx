e20979e36134b15205cec177b45660a844a46eea
Aiden <hyena.shenanigans@gmail.com>
2022-10-13 22:04:00 -0700

    Adds body form and working method calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,98 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useReducer } from 'react';
+//import axios from 'axios';
 import './App.scss';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
 import { Results } from './components/results/results';
 import { Form } from './components/form/form';
 
-const App = () => {
-  let [state, setState] = useState({});
-  const callApi = async (request: any) => {
-    console.log(`it's callback time`, request)
-    //let data;
-    // switch (request?.method) {// axios.request
-    //   case 'GET':
-    //     data = await axios.get(request.url);
-    //     break;
-    //   case 'POST':
-    //     data = await axios.post(request.url);
-    //     break;
-    //   case 'PUT':
-    //     data = await axios.put(request.url);
-    //     break;
-    //   case 'DELETE':
-    //     data = await axios.delete(request.url);
-    //     break;
+interface AppState {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  url: string;
+  body: object | undefined;
+  response: object | undefined;
+}
 
-    //   default:
-    //     console.log('No methods?');
-    //     break;
-    // }
-    let response = await fetch(request.url, {
-      method: request.method,
-    });
+export const changeMetadata = (state: AppState, request: any) => {
+    state.method = request.method;
+    state.url = request.url;
+
+  return { ...state, request, };
+}
+
+export const setBodyIfNotGet = (state: AppState, body: object) => {
+  if (state.method !== 'GET') {
+    state.body = body;
+  }
+  return state;
+}
+
+export const setData = (state: AppState, response: object) => {
+  state.response = response;
+  return state;
+}
+
+export const reset = (state: AppState) => {
+  console.log('resetting');
+  state.response = undefined;
+  return state;
+}
+
+export const dispatchAction = (state: AppState, action: { action: string, body: unknown }) => {
+  switch (action.action) {
+    case 'meta':
+      console.log('metadata')
+      return changeMetadata(state, action.body as object);
+    case 'body':
+      return setBodyIfNotGet(state, action.body as object);
+    case 'response':
+      return setData(state, action.body as object);
+    case 'reset':
+      return reset(state);
+    default:
+      return state;
+  }
+}
+
+const App = () => {
+  //let [state, setState] = useState({});
+  const [state, dispatch] = useReducer(dispatchAction, {
+    method: 'GET',
+    url: 'http://localhost:3000',
+    body: undefined,
+    response: undefined,
+  })
+
+  const updateBodyIfNotGet = (body: object) => {
+    dispatch({action: 'body', body});
+  }
+
+  const updateMetadata = (request: any) => {
+    dispatch({action: 'meta', body: request});
+  }
+
+  const updateData = (response: any) => {
+    dispatch({action: 'response', body: response});
+  }
+
+  const reset = () => {
+    dispatch({action: 'reset', body: undefined});
+  }
+
+  const callApi = async (request: any) => {
+    console.log('request:', request)
+    //reset();
+    updateMetadata(request);
+    let response;
+    if(request.method === 'GET') {
+      response = await fetch(request.url, { method: request.method })
+    } else {
+      updateBodyIfNotGet(request.body);
+      response = await fetch(request.url, { method: request.method, body: request.body });
+    }
     let data = await response.json();
-    //console.log(data)
-    state = { data, request };
-    setState(state);
+    console.log(data) // reponse from a get is working but not displaying
+    updateData(data);
   }
   return (
     <>
--- src/components/results/results.tsx
+++ src/components/results/results.tsx
@@ -1,2 +1,2 @@
 import './results_style.scss';
-export const Results = (props: any) => (<section><pre>{props.results.data ? JSON.stringify(props.results, undefined, 2): null}</pre></section>);
+export const Results = (props: any) => (<section><pre>{props.results.response ? JSON.stringify(props.results, undefined, 2): null}</pre></section>);
